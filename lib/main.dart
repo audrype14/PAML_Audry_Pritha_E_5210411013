d14d73f84cec1a6264a87accf324fe0358132b0c
audrype14 <audry.pritha@gmail.com>
2024-11-06 18:30:57 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:paml_flutter_responsi/src/constants/color.dart';
 void main() {
   runApp(const MyApp());
 }
-
+ 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
