86ed4b6590f7f2607a6ea72238b95211d2004c29
audrype14 <audry.pritha@gmail.com>
2024-11-06 19:47:02 +0700

    update

--- lib/src/data/profile.dart
+++ lib/src/data/profile.dart
@@ -3,5 +3,5 @@ import 'package:paml_flutter_responsi/src/models/profile.dart';
 final Profile profileData = Profile(
   name: "Audry Pritha Ekaharyani",
   nim: "5210411013",
-  imageUrl: "https://cdns.klimg.com/resized/670x/g/8/_/8_fakta_tersembunyi_tentang_eren_yeager_yang_jarang_diketahui_penggemar_ungkap_sisi_lain_sang_protagonis/p/eren_yaeger-20240830-006-non_fotografer_kly.jpg",
+  imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cleanpng.com%2Fpng-cartoon-woman-face-brown-hair-brown-eyes-round-fac-8053139%2F4.html&psig=AOvVaw1iFfas2N9QTtDUnEU4FVqX&ust=1730983503776000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPjcppjex4kDFQAAAAAdAAAAABAJ",
 );
